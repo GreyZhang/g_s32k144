7e281213c286008f1c0199c5932db0c0e4adeb78
greyzhang <greyzhang@126.com>
2020-03-29 17:04:55 +0800

    Add a new learning note about CAN receive basic function.

---
+++ S32K144_029_CAN_Receive/can_lld.h
@@ -0,0 +1,12 @@
+#ifndef CAN_LLD_H
+#define CAN_LLD_H
+
+#include "canCom1.h"
+
+#define RX_MSG_ID 0x100U
+
+void can_lld_init(void);
+void can_lld_step(void);
+void can_lld_tx(uint32_t mailbox, uint32_t messageId, uint8_t * data, uint32_t len);
+
+#endif
